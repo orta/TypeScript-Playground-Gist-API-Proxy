ab86f0a9c4fcbd4f02c5be8d519966ffef27e69b
Orta Therox <git@orta.io>
2021-09-19 10:28:08 +0100

    Adds the md ->> html pipeline

--- API/index.ts
+++ API/index.ts
@@ -1,6 +1,9 @@
 import { AzureFunction, Context, HttpRequest } from "@azure/functions";
 import { Octokit } from "@octokit/rest";
 import { createGitHubClient } from "./createGitHubClient";
+import type { Endpoints } from "@octokit/types/dist-types/generated/endpoints";
+
+export type GistFiles = Endpoints["GET /gists/{gist_id}"]["response"]["data"]["files"];
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
   const api = createGitHubClient();
@@ -24,6 +27,7 @@ type Success =
     }
   | {
       type: "story";
+      title: string;
       files: Array<StoryContent>;
     };
 
@@ -36,9 +40,10 @@ const createResponse = (context: Context) => (code: number, response: APIRespons
   };
 };
 
+// Grabs the gist, if it contains  1 file then we validate that is code and return that
+//                 if it contains <1 files, then we call it a 'story' and support a file browser and renderer HTML + code
 export const get = async function (api: Octokit, context: Context, req: HttpRequest): Promise<void> {
   const res = createResponse(context);
-
   const gistID = req.query["gistID"];
   // Gist IDs are global, so any gist is legit
   if (!gistID) return res(401, { error: true, display: "Request for gist did not include an ID " });
@@ -47,7 +52,7 @@ export const get = async function (api: Octokit, context: Context, req: HttpRequ
   if (gist.status !== 200) return res(gist.status, { error: true, display: `Could not find gist with ID ${gistID}` });
 
   const files = Object.keys(gist.data.files);
-  if (files.length === 0) return res(404, { error: true, display: `There were no files in gist: ${gistID}` });
+  if (files.length === 0) return res(404, { error: true, display: `There are no files in gist: ${gistID}` });
 
   if (files.length === 1) {
     // We're a single file example
@@ -56,16 +61,20 @@ export const get = async function (api: Octokit, context: Context, req: HttpRequ
       return res(404, { error: true, display: `Cannot render ${file.filename} in the playground as it's not code` });
     return res(200, { type: "code", code: file.content, params: "" });
   } else {
-    // WIP
-    const file = gist.data.files[files[0]];
-    return res(200, { type: "story", files: [] });
+    const files = await filesToStoryPages(gist.data.files, mdToHTML(api));
+    const title = gist.data.description;
+    if ("error" in files) {
+      return res(400, files);
+    } else {
+      return res(200, { type: "story", files, title });
+    }
   }
 };
 
 type NewPostLink = { state: "success"; id: string; user: string; jwt: string };
 
 export const post = async function (api: Octokit, context: Context, req: HttpRequest): Promise<void> {
-  // For new one,
+  // For posting
 };
 
 export const canShowAsCode = (filename: string) => {
@@ -74,7 +83,7 @@ export const canShowAsCode = (filename: string) => {
   return known.indexOf(filetype) !== -1;
 };
 
-export const contentToCodePlusCompilerSettings = (contents: string) => {
+export const contentToCodePlusCompilerSettings = (extension: string, contents: string) => {
   let compiler = {};
   if (contents.startsWith("//// {")) {
     // convert windows newlines to linux new lines
@@ -90,10 +99,69 @@ export const contentToCodePlusCompilerSettings = (contents: string) => {
       return null;
     }
   }
+  if (extension.startsWith("j")) {
+    compiler["filetype"] = "js";
+  }
+
   return {
     code: contents,
-    compilerOptions: compiler,
+    compilerOptions: new URLSearchParams(compiler).toString(),
   };
+};
+
+const mdToHTML =
+  (api: Octokit) =>
+  async (md: string): Promise<string | null> => {
+    const req = await api.markdown.render({ text: md });
+    if (req.status === 200) {
+      return req.data;
+    }
+    return null;
+  };
+
+type MDToHTML = (md: string) => Promise<string | null>;
+
+export const filesToStoryPages = async (
+  files: GistFiles,
+  mdToHTML: MDToHTML
+): Promise<StoryContent[] | { error: true; display: string }> => {
+  const items: StoryContent[] = [];
+
+  const keys = Object.keys(files).sort();
+  for (const key of keys) {
+    if (!key.includes("~")) return { error: true, display: `The file '${key}' does not start with an index and ~ - e.g. '1~'` };
+    const [indexStr, filename] = key.split("~");
+    const index = Number(indexStr);
+
+    if (!isFinite(index)) return { error: true, display: `The file '${key}' does not start with a number before the '~'` };
+
+    const content = files[key].content;
+    if (!content) return { error: true, display: `The file '${key}' is empty` };
+
+    const ext = filename.split(".").pop();
+    if (canShowAsCode(ext)) {
+      const preparse = contentToCodePlusCompilerSettings(ext, content);
+      items[index] = { type: "code", code: preparse.code, params: preparse.compilerOptions, title: filename.split("~")[0].trim() };
+    } else if (ext === "md") {
+      const html = await mdToHTML(content);
+      if (!html) return { error: true, display: `The markdown for '${key}' could not be rendered by the GitHub Markdown API` };
+
+      items[index] = { type: "html", html, title: filename.split("~")[0].trim() };
+    } else {
+      return {
+        error: true,
+        display: `Can't do anything with the file '${key}'. It needs to be a markdown file, or something the playground supports`,
+      };
+    }
+  }
+
+  // Fill in the gaps
+  for (let index = 0; index < items.length; index++) {
+    const element = items[index];
+    if (!element) items[index] = { type: "hr" };
+  }
+
+  return items;
 };
 
 export default httpTrigger;
--- api.test.ts
+++ api.test.ts
@@ -1,4 +1,4 @@
-import { contentToCodePlusCompilerSettings, get } from "./API/index";
+import { contentToCodePlusCompilerSettings, filesToStoryPages, get } from "./API/index";
 import { createMockGitHubClient } from "./utils/createMockGitHubAPI";
 
 const runTest = async (req: any, ctx: any, mock: (api: import("./utils/createMockGitHubAPI").MockAPI) => void) => {
@@ -49,7 +49,7 @@ describe(get, () => {
     expect(res).toMatchInlineSnapshot(`
 {
   "body": {
-    "display": "There were no files in gist: 1234",
+    "display": "There are no files in gist: 1234",
     "error": true,
   },
   "code": 404,
@@ -103,24 +103,146 @@ describe(get, () => {
   });
 });
 
+describe("playground stories", () => {
+  it("it bails when it already", async () => {
+    const res = await runTest({ query: { gistID: "1234" } }, {}, (mockAPI) => {
+      const files = {
+        "1~Intro.md": {
+          filename: "1~Intro.md",
+          content: "## welcome",
+        },
+      };
+      mockAPI.gists.get.mockResolvedValueOnce({ status: 200, data: { files } });
+    });
+
+    expect(res).toMatchInlineSnapshot(`
+{
+  "body": {
+    "display": "Cannot render 1~Intro.md in the playground as it's not code",
+    "error": true,
+  },
+  "code": 404,
+}
+`);
+  });
+});
+
 describe(contentToCodePlusCompilerSettings, () => {
   it("gives an empty compiler opts", () => {
-    expect(contentToCodePlusCompilerSettings("const a = 123")).toMatchInlineSnapshot(`
+    expect(contentToCodePlusCompilerSettings("ts", "const a = 123")).toMatchInlineSnapshot(`
 {
   "code": "const a = 123",
-  "compilerOptions": {},
+  "compilerOptions": "",
 }
 `);
   });
 
   it("re-uses the format from the typescript website", () => {
-    expect(contentToCodePlusCompilerSettings("//// { compiler: { strictFunctionTypes: false } }\nconst a = 123")).toMatchInlineSnapshot(`
+    expect(contentToCodePlusCompilerSettings("ts", "//// { compiler: { strictFunctionTypes: false } }\nconst a = 123"))
+      .toMatchInlineSnapshot(`
 {
   "code": "const a = 123",
-  "compilerOptions": {
-    "strictFunctionTypes": false,
-  },
+  "compilerOptions": "strictFunctionTypes=false",
 }
+`);
+  });
+
+  it("adds the JS settings to the params", () => {
+    expect(contentToCodePlusCompilerSettings("js", "const a = 123").compilerOptions).toMatchInlineSnapshot(`"filetype=js"`);
+  });
+});
+
+const f = (files: any): any => {
+  const gist: any = {};
+  Object.keys(files).forEach((f) => {
+    gist[f] = {
+      filename: f,
+      content: files[f],
+    };
+  });
+  return gist;
+};
+
+const noOpMDToHTML = async () => " ";
+const mdToHTML = async (md) => `html version of ${md}`;
+
+describe(filesToStoryPages, () => {
+  it("bails when a filename does not start with a number and ~", async () => {
+    const files = {
+      "abc.ts": "// code",
+    };
+    const res = await filesToStoryPages(f(files), noOpMDToHTML);
+    expect(res).toMatchInlineSnapshot(`
+{
+  "display": "The file 'abc.ts' does not start with an index and ~ - e.g. '1~'",
+  "error": true,
+}
+`);
+  });
+
+  it("maps code correctly", async () => {
+    const files = {
+      "0 ~ abc.ts": "// code",
+    };
+    const res = await filesToStoryPages(f(files), noOpMDToHTML);
+    expect(res).toMatchInlineSnapshot(`
+[
+  {
+    "code": "// code",
+    "params": "",
+    "title": "abc.ts",
+    "type": "code",
+  },
+]
+`);
+  });
+
+  it("maps md correctly", async () => {
+    const files = {
+      "0 ~ abc.md": "## Markdown",
+    };
+    const res = await filesToStoryPages(f(files), mdToHTML);
+    expect(res).toMatchInlineSnapshot(`
+[
+  {
+    "html": "html version of ## Markdown",
+    "title": "abc.md",
+    "type": "html",
+  },
+]
+`);
+  });
+
+  it("fills gaps in with HR", async () => {
+    const files = {
+      "0 ~ abc.ts": "// code",
+      "1 ~ file.md": "/* md */",
+      "3 ~ abc2.ts": "// code",
+    };
+    const res = await filesToStoryPages(f(files), mdToHTML);
+    expect(res).toMatchInlineSnapshot(`
+[
+  {
+    "code": "// code",
+    "params": "",
+    "title": "abc.ts",
+    "type": "code",
+  },
+  {
+    "html": "html version of /* md */",
+    "title": "file.md",
+    "type": "html",
+  },
+  {
+    "type": "hr",
+  },
+  {
+    "code": "// code",
+    "params": "",
+    "title": "abc2.ts",
+    "type": "code",
+  },
+]
 `);
   });
 });
